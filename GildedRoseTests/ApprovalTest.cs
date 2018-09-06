d7b1e9a7ece10b725f3a3f67fdae8c7f781513c0
Aleksandar Budić <aleksandar.budic@nultien.rs>
2018-09-06 09:55:39 +0200

    init

---
+++ GildedRoseTests/ApprovalTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using GildedRoseTavern;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            var lines = File.ReadAllLines("ThirtyDays.txt");
+
+            StringBuilder fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            String output = fakeoutput.ToString();
+
+            var outputLines = output.Split(Environment.NewLine);
+            for (var i = 0; i < Math.Min(lines.Length, outputLines.Length); i++)
+            {
+                Assert.Equal(lines[i], outputLines[i]);
+            }
+        }
+    }
+}
